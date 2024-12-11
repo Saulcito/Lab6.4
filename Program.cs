33d4eb736fc34442b82e27eeb5a6646e4773e4be
Saul <Saul@DESKTOP-D5AGNQ5>
2024-12-11 16:44:50 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6._4
+{
+    internal class Program
+    {
+        static double Power(double M, int N) 
+        {
+            if (N == 0)
+            {
+                return 1;
+            }
+            else if (N > 0)
+            {
+                return M * Power(M, N - 1);
+            }
+            else 
+            {
+                return 1 / Power(M, -N);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the base numer (M):");
+            double M = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the exponent number (N):");
+            int N = int.Parse(Console.ReadLine());
+
+            double result = Power(M, N);
+
+            Console.WriteLine($"\n {M} raised to the power of {N} is: {result}");
+
+            Console.ReadLine();
+        }
+    }
+}
